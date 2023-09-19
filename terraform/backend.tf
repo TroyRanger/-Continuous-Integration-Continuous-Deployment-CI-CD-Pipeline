3282f5eb06dbf0708335d47cfcf9b030929f2e37
TroyRanger <137522820+TroyRanger@users.noreply.github.com>
2023-09-19 20:41:45 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "terraform.tfstate"
     region         = "us-east-1" # Replace with your desired region
     encrypt        = true
-    dynamodb_table = "my-lock-table" # Optional: Use DynamoDB for state locking
+    
   }
 }
