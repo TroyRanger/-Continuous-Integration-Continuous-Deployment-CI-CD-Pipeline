663daff913c3191d6162286cabe00469d3c10693
TroyRanger <137522820+TroyRanger@users.noreply.github.com>
2023-09-19 20:40:57 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "web_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # Replace with your desired AMI
-  instance_type = "t2.micro" # Replace with your desired instance type
-  subnet_id     = aws_subnet.subnet_a.id # Use the appropriate subnet
-  key_name      = "my-key-pair" # Replace with your key pair name
+resource "ec2 instance" "Hello World" {
+  ami           = "ami-0989fb15ce71ba39e" # Replace with your desired AMI
+  instance_type = "t3.micro" # Replace with your desired instance type
+  subnet_id     = subnet-013994aad1156e810 # Use the appropriate subnet
+  key_name      = "HelloWorld" # Replace with your key pair name
 }
 
 # Define security groups, EBS volumes, and other instance configurations here
