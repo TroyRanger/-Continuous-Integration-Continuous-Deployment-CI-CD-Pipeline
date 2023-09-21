a02ae6198bc5178b52052d3b1855400facc110cb
TroyRanger <137522820+TroyRanger@users.noreply.github.com>
2023-09-21 22:06:51 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "troyrangerbucket" # Replace with your bucket name
+    bucket         = "arn:aws:s3:::troyrangerbucket" # Replace with your bucket name
     key            = "terraform.tfstate"
     region         = "us-east-1" # Replace with your desired region
     encrypt        = true
