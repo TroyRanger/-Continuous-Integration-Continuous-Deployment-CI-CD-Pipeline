a231a49a41efddf30e6418ac88e722bfc6cea22c
TroyRanger <137522820+TroyRanger@users.noreply.github.com>
2023-09-21 22:16:56 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web_server" {
   ami           = "ami-0989fb15ce71ba39e" # Replace with your desired AMI
   instance_type = "t3.micro" # Replace with your desired instance type
-  subnet_id     = subnet-013994aad1156e810 # Use the appropriate subnet
+  
   key_name      = "HelloWorld" # Replace with your key pair name
 }
 
