4fff9a8dd6c1b12c8cdfc82739b2ea8421581cef
TroyRanger <137522820+TroyRanger@users.noreply.github.com>
2023-09-21 22:15:02 +0530

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "arn:aws:s3:::troyrangerbucket" # Replace with your bucket name
-    key            = "terraform.tfstate"
-    region         = "us-east-1" # Replace with your desired region
-    encrypt        = true
-    
-  }
-}
